bf19a991913e53d75232be3a13828c851c1c8cc1
Anuraj Bhardwaj <anurajbhardwajofficial@gmail.com>
2026-07-05 12:25:33 +0530

    Completed Day 8: Premium gallery, testimonials, animations and UI polish

---
+++ animations.js
@@ -0,0 +1,21 @@
+const observer = new IntersectionObserver((entries)=>{
+
+    entries.forEach(entry=>{
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+},{
+    threshold:0.2
+});
+
+document.querySelectorAll(".gallery-item,.review-card").forEach((el)=>{
+
+    observer.observe(el);
+
+});
